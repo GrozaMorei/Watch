2aae59e84d935913101bfc5061f6444e62a7a1a4
GrozaMorei <81432133+GrozaMorei@users.noreply.github.com>
2024-04-09 12:20:14 +0300

    Update index.js
    Исправлены некоторые баги.

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ let realHource;
 let realMinute;
 let realSecond;
 
-arrowHour.style.transform = `rotateZ(${date.getHours() * 30 + date.getMinutes() / 2}deg)`;
+arrowHour.style.transform = `rotateZ(${
+	date.getHours() * 30 + date.getMinutes() / 2
+}deg)`;
 arrowMinute.style.transform = `rotateZ(${date.getMinutes() * 6}deg)`;
-
-let rotate = 0;
+animReset(1);
 
 setInterval(() => {
 	date = new Date();
@@ -19,22 +20,26 @@ setInterval(() => {
 	realSecond = date.getSeconds();
 
 	if (realMinute == 0) {
-		arrowMinute.style.transition = 'none';
+		animReset(0);
 		arrowMinute.style.transform = `rotateZ(0deg)`;
-		arrowMinute.style.transition = 'all 60s';
-	} else {
-		arrowMinute.style.transform = `rotateZ(${realMinute * 6}deg)`;
+		animReset(1);
 	}
-
 	if (realHource == 0) {
-		arrowHour.style.transition = 'none';
+		animReset(0);
 		arrowHour.style.transform = `rotateZ(0deg)`;
-		arrowHour.style.transition = 'all 60s';
-	} else {
-		arrowHour.style.transform = `rotateZ(${
-			realHource * 30 + realMinute / 2
-		}deg)`;
+		animReset(1);
 	}
+	arrowMinute.style.transform = `rotateZ(${realMinute * 6}deg)`;
+	arrowHour.style.transform = `rotateZ(${realHource * 30 + realMinute / 2}deg)`;
 
 	time.innerHTML = `${realHource}:${realMinute}:${realSecond}`;
 }, 1000);
+
+function animReset(value) {
+	value
+		? (arrowHour.style.transition = 'all 60s')
+		: (arrowHour.style.transition = 'none');
+	value
+		? (arrowMinute.style.transition = 'all 60s')
+		: (arrowMinute.style.transition = 'none');
+}
